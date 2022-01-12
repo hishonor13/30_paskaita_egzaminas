3e8b1df0f274ba421ccd82ff8aecdb88ec1e31dd
Mantas Kelminskas <mantas.kelminskas@gmail.com>
2022-01-12 21:44:34 +0200

    8 uzduoty atlikti papildomi neprasyti veiksmai

--- Task 8/script.js
+++ Task 8/script.js
@@ -23,10 +23,44 @@ function Calculator (num1, num2) {
     }
 }
 
-let skaicius1 = 16;
-let skaicius2 = 8;
-let myCalculator = new Calculator(skaicius1, skaicius2)
-console.log(myCalculator.sum());
-console.log(myCalculator.subtraction());
-console.log(myCalculator.multiplication());
-console.log(myCalculator.division());
+// let skaicius1 = 16;
+// let skaicius2 = 8;
+// let myCalculator = new Calculator(skaicius1, skaicius2)
+// console.log(myCalculator.sum());
+// console.log(myCalculator.subtraction());
+// console.log(myCalculator.multiplication());
+// console.log(myCalculator.division());
+
+document.querySelector("form").addEventListener("submit", (e) => {
+    e.preventDefault();
+    let num1 = parseInt(document.querySelector("input[name=number1]").value);
+    let num2 = parseInt(document.querySelector("input[name=number2]").value);
+    let selectas = document.getElementById("zzz");
+    let procedura = selectas.options[selectas.selectedIndex].value;
+    let myCalculator = new Calculator(num1, num2)
+    let myH1 = document.createElement("h1");
+    document.querySelector("#ats").innerHTML = "";
+    let data = "";
+    switch(procedura){
+        case "sum":
+            data = myCalculator.sum();
+            myH1.append(data);
+            ats.append(myH1);
+            break;
+        case "subtraction":
+            data = myCalculator.subtraction();
+            myH1.append(data);
+            ats.append(myH1);
+            break;
+        case "multiplication":
+            data = myCalculator.multiplication();
+            myH1.append(data);
+            ats.append(myH1);
+            break;
+        case "division":
+            data = myCalculator.division();
+            myH1.append(data);
+            ats.append(myH1);
+            break;
+    }
+})
