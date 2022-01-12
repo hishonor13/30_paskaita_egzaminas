67d970ec1fbd7a1a5f43ac386acc58ad18aacb20
Mantas Kelminskas <mantas.kelminskas@gmail.com>
2022-01-12 17:50:07 +0200

    Atlikta 8 uzduotis

--- Task 8/script.js
+++ Task 8/script.js
@@ -5,3 +5,26 @@ subtraction() - priima du skaičius ir grąžina jų skirtumą.
 multiplication() - priima du skaičius ir grąžina jų daugybos rezultatą;
 division() - priima du skaičius ir grąžina jų dalybos rezultatą;
 ------------------------------------------------------------------------------------ */
+
+function Calculator (num1, num2) {
+    this.num1 = num1;
+    this.num2 = num2;
+    this.sum = function (){
+        return num1 + num2;
+    }
+    this.subtraction = function (){
+        return num1 - num2;
+    }
+    this.multiplication = function (){
+        return num1 * num2;
+    }
+    this.division = function (){
+        return num1 / num2;
+    }
+}
+
+let myCalculator = new Calculator(16, 8)
+console.log(myCalculator.sum());
+console.log(myCalculator.subtraction());
+console.log(myCalculator.multiplication());
+console.log(myCalculator.division());
