50eb5ddd2d4275159197224cf187a090fcfd99cf
Mantas Kelminskas <mantas.kelminskas@gmail.com>
2022-01-12 18:19:15 +0200

    Atlikta 10 uzduotis

--- Task 10/script.js
+++ Task 10/script.js
@@ -2,6 +2,11 @@
 Sutvarkykite užduoties "Task 10" esančius failus taip, kad veiktų žemiau pateiktos funcijos
 -------------------------------------------------------------------------------------------- */
 
+import { composition } from "./modules/math/composition.js"
+import { division } from "./modules/math/division.js"
+import { substraction } from "./modules/math/subtraction.js"
+import { multiplication } from "./modules/math/multiplication.js"
+
 let a = composition(one, four);
 let b = division(four, two);
 let c = substraction(three, two);
@@ -10,4 +15,4 @@ let d = multiplication(five, two);
 console.log(a);
 console.log(b);
 console.log(c);
-console.log(c);
+console.log(d);
